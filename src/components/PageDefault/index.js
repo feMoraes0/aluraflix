17a1de96ac9b75d8735d80f2e91340899d235f32
feMoraes0 <fernandomoraes.lopes@gmail.com>
2020-07-28 18:10:32 -0300

    Added page default component

---
+++ src/components/PageDefault/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Menu from '../Menu';
+import Footer from '../Footer';
+
+const PageDefault = ({children}) => {
+  return (
+    <>
+      <Menu />
+        {children}
+      <Footer />
+    </>
+  );
+}
+
+export default PageDefault;
--- src/pages/CadastroVideo/index.js
+++ src/pages/CadastroVideo/index.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import PageDefault from '../../components/PageDefault';
 
 const CadastroVideo = () => {
-  return (<p>Pagina de cadastro de videos</p>);
+  return (
+    <PageDefault>
+      <p>Pagina de cadastro de videos</p>
+    </PageDefault>
+  );
 }
 
 export default CadastroVideo;
